c16de8311014d5ff1e92691028b3c936ca709653
colinmorris <colin.morris2@gmail.com>
2019-07-24 15:25:52 -0400

    polcounts css, expander

--- src/PolCounts.js
+++ src/PolCounts.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 import * as dp from './display_helpers';
 
+import './polcounts.css';
+
 class PolCounts extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    }
+    this.limit = 15;
+    this.collapse = this.collapse.bind(this);
+    this.expand = this.expand.bind(this);
+  }
+
+  collapse() {
+    this.setState({expanded: false});
+  }
+  expand() {
+    this.setState({expanded: true});
+  }
 
   render() {
     // Sort in descending order of count
-    const lis = Object.entries(this.props.counts)
+    var lis = Object.entries(this.props.counts)
       .sort( (a, b) => b[1] - a[1])
       .map(ent => {
       let pol=ent[0], count=ent[1];
@@ -16,10 +35,19 @@ class PolCounts extends React.Component {
           {count}
           </li>);
     });
+    var resizer = '';
+    if (this.state.expanded) {
+      resizer = <button onClick={this.collapse}>-</button>;
+    } else if (this.limit < Object.keys(this.props.counts).length) {
+      lis = lis.slice(0, this.limit);
+      resizer = <button onClick={this.expand}>+</button>;
+    }
     return (
-      <ul>
+      <div>
+      <ul className="horiz">
         {lis}
-      </ul>
+      </ul>{resizer}
+      </div>
       );
   }
 }
