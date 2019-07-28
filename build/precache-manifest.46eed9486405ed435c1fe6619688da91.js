c6fca7afb196a9f2e50388c7adbe0ca059c2da1a
colinmorris <colin.morris2@gmail.com>
2019-07-27 21:41:39 -0400

    hack

---
+++ build/precache-manifest.46eed9486405ed435c1fe6619688da91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0b83b6e2f3cb5ba48def4db47b9d44",
+    "url": "/rmstats/index.html"
+  },
+  {
+    "revision": "37f0da3684fc4192f927",
+    "url": "/rmstats/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "741fb1c1035bb1c2b3d4",
+    "url": "/rmstats/static/css/main.b24255fb.chunk.css"
+  },
+  {
+    "revision": "37f0da3684fc4192f927",
+    "url": "/rmstats/static/js/2.20f27480.chunk.js"
+  },
+  {
+    "revision": "741fb1c1035bb1c2b3d4",
+    "url": "/rmstats/static/js/main.b15d80cd.chunk.js"
+  },
+  {
+    "revision": "3bdae3365c6f32344055",
+    "url": "/rmstats/static/js/runtime~main.4e476c31.js"
+  }
+]);
