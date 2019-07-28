908d4fe505048087dadf9f9bd9f91bfe1f874407
colinmorris <colin.morris2@gmail.com>
2019-07-27 23:37:57 -0400

    build stuff

---
+++ build/precache-manifest.9967be60c5e0dd5f2a4cda73dce0edb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a708b7632ba1abdb7510737f40214c5b",
+    "url": "/rmstats/index.html"
+  },
+  {
+    "revision": "37f0da3684fc4192f927",
+    "url": "/rmstats/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b033993e9f7946a0d7cc",
+    "url": "/rmstats/static/css/main.b24255fb.chunk.css"
+  },
+  {
+    "revision": "37f0da3684fc4192f927",
+    "url": "/rmstats/static/js/2.20f27480.chunk.js"
+  },
+  {
+    "revision": "b033993e9f7946a0d7cc",
+    "url": "/rmstats/static/js/main.713fe980.chunk.js"
+  },
+  {
+    "revision": "3bdae3365c6f32344055",
+    "url": "/rmstats/static/js/runtime~main.4e476c31.js"
+  }
+]);
